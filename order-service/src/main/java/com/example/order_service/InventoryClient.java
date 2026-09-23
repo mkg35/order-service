ba108f8f57f9adcee4a781dbce3aed2deda2aea1
melih <gulerm55@gmail.com>
2026-09-23 16:56:50 +0300

    feat: Eureka Service Discovery ve LoadBalancer ile dinamik iletisim kuruldu

--- order-service/src/main/java/com/example/order_service/InventoryClient.java
+++ order-service/src/main/java/com/example/order_service/InventoryClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "inventory", url = "http://localhost:8082")
+@FeignClient(name = "inventory-service")
 public interface InventoryClient {
     @GetMapping("/api/inventory/{skuCode}")
     public Boolean isInStock(@PathVariable String skuCode);
