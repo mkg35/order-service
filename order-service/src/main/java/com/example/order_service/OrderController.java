b54244852565e5799c94e0735ec42071ca3cf66d
melih <gulerm55@gmail.com>
2026-09-24 23:39:27 +0300

    feat: Config Server'a bağlandı ve yerel ayarlar temizlendi

--- order-service/src/main/java/com/example/order_service/OrderController.java
+++ order-service/src/main/java/com/example/order_service/OrderController.java
@@ -3,10 +3,7 @@ package com.example.order_service;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
